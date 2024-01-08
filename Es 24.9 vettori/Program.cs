d727544c3c733075d8085ac653a1f61d881e2ede
MariannaBazzani <merionbaz@gmail.com>
2024-01-08 18:35:47 +0100

    Aggiungere i file di progetto.

---
+++ Es 24.9 vettori/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_24._9_vettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 2);
+            Vector v2 = new Vector(3, 3);
+
+            //Vector v3 = new Vector();
+            //Console.WriteLine("inserisci le coordinata di un terzo vettore: ");
+            //v3 = Vector.Parse(Console.ReadLine());
+
+            Vector vectorR = v1.Somma(v2);
+            Console.WriteLine($"\nSomma del primo e del secondo vettore: {vectorR.Xvector};{vectorR.Yvector}");
+        }
+    }
+}
---
+++ Es 24.9 vettori/Vector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_24._9_vettori
+{
+    internal class Vector
+    {
+        public double Xvector { get; set; }
+        public double Yvector { get; set; }
+        
+        public Vector() { }
+        public Vector(double x, double y)
+        {
+            this.Xvector = x;
+            this.Yvector = y;
+        }
+
+        public Vector Somma(Vector V)
+        {
+            Vector Vresult = new Vector();
+            Vresult.Xvector = V.Xvector + Xvector;
+            Vresult.Yvector = V.Yvector + Yvector;
+            
+            return Vresult;
+        }
+    }
+
+
+}
